d99be233beadd464eccca46189d3c0e62e715500
prog4food <3475861+prog4food@users.noreply.github.com>
2022-10-13 19:50:18 +0300

    samsung loader fixes App update Oll.tv: + Orsay, Panasonic, Sony, Hisense

--- html/ott-play-maple-loader.js
+++ html/ott-play-maple-loader.js
@@ -1,10 +1,17 @@
-var host = 'http://ott-play.com:80/samsung/maple/';
-if (navigator.userAgent.toLowerCase().search(/tizen/) > 0) {
-    var loader = document.createElement('script');
-    loader.type = 'text/javascript';
-    loader.async = false;
-    loader.src = host + 'loader.js';
-    document.head.appendChild(loader)
+var host = 'http://ott-play.com';
+if (navigator.userAgent.toLowerCase().search(/tizen/) == -1) {
+    window.location = host + '/samsung/maple/';
 } else {
-    window.location = host
+    (function() {
+        var script = document.createElement('SCRIPT');
+        script.src  = host+'/js/jq-1.12.4.js?1';
+        script.type = 'text/javascript';
+        script.onload = function() {
+            $(document.body).load(host+'/stbPlayer/stbPlayer_body.html');
+            $.getScript(host+'/samsung/tizen/stb.js', function(){
+                $.getScript(host+'/stbPlayer/stbPlayer.js');
+            });
+        };
+        document.getElementsByTagName('head')[0].appendChild(script);
+    })();
 }
