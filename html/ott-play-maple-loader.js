5a672969d71fcfa6a353df38114898ffa617c6e2
prog4food <3475861+prog4food@users.noreply.github.com>
2023-02-26 17:32:11 +0300

    + финальное обновление загрузчика для tizen % обновление информации о совместимости

--- html/ott-play-maple-loader.js
+++ html/ott-play-maple-loader.js
@@ -1,17 +1,34 @@
-var host = 'http://ott-play.com';
-if (navigator.userAgent.toLowerCase().search(/tizen/) == -1) {
-    window.location = host + '/samsung/maple/';
+var host = 'ott-play.com', ott_device = 'samsung/tizen';
+var client_can_https;
+
+if (navigator.userAgent.toLowerCase().indexOf('tizen') == -1) {
+    // Orsay: грузим по ссылке, они умеют
+    window.location = 'http://' + host + '/f/samsung/maple/';
 } else {
-    (function() {
-        var script = document.createElement('SCRIPT');
-        script.src  = host+'/js/jq-1.12.4.js?1';
-        script.type = 'text/javascript';
-        script.onload = function() {
-            $(document.body).load(host+'/stbPlayer/stbPlayer_body.html');
-            $.getScript(host+'/samsung/tizen/stb.js', function(){
-                $.getScript(host+'/stbPlayer/stbPlayer.js');
-            });
+    // Если вдруг Tizen: подменяем тело приложения, иначе потеряем window.tizen
+    (function () {
+        function cutProto(_s) { var _i = _s.indexOf('://'); return (_i === -1 ? _s : _s.slice(_i+3)); }
+        function lLoader(_mode) {
+            if (host) { host = 'http://' + cutProto(host); }
+            if (_mode > 0) { client_can_https = (_mode === 2); }
+            var scriptTag = document.createElement('script');
+            scriptTag.src = host + '/loader/inplace.js?' + Math.random().toString();
+            scriptTag.type = 'text/javascript';
+            scriptTag.onerror = function (e) {
+                console.error(e);
+                document.body.innerHTML = '<h3>Error loading: ' + e.target.src + '</h3><br/><pre>Error loading: ' + e.target.src + '</pre>';
+            }
+            document.head.appendChild(scriptTag);
         };
-        document.getElementsByTagName('head')[0].appendChild(script);
+        try {
+            // Check HTTPS connectivity
+            var _xhr = new XMLHttpRequest();
+            _xhr.open('GET', 'https://s.ottp.eu.org/generate_204', true);
+            _xhr.onerror = function () { lLoader(1); };
+            _xhr.onreadystatechange = function () { if (_xhr.readyState === 4 && _xhr.status === 204) { lLoader(2); } };
+            _xhr.send();
+        } catch(e) {
+            lLoader(0);
+        }
     })();
 }
